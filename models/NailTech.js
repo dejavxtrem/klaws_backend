4d84e499e0c8daaa5655d436dd3e3544683cbd84
Dejay <dejavxtrem@hotmail.com>
2020-08-16 18:38:36 -0500

    new construct

--- models/NailTech.js
+++ models/NailTech.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 
-const SALD_ROUNDS = 6;
-
-const NailTechs = mongoose.Schema({
-    shopName: {type: String, required: true},
-    address: {type: String, required:true},
-    city: {type:String, required:true},
-
-
+const serviceSchema = new mongoose.Schema({
+    serviceType: {type: String, required: true},
+    service: {
+        serviceName: {type: String, required: true},
+        address: {type: String, required: true},
+        servicePrice: {type: String, required: true},
+        serviceTime: {type: String, required: true},
+        servicePhoto: {type: String, required: true}
+    }
 })
+
+
+const NailTech = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    salonname: {type: String, required:true},
+    password: {type: String, required:true},
+    serviceType: [serviceSchema]
+}, {timestamp: true})
+
+
---
+++ routes/userAuthRoute.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const User = require('../models/User')
+
+
