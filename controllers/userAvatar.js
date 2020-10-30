d9f5c402b255a41ab13746aa8600d77f9f0d84fe
Dejay <dejavxtrem@hotmail.com>
2020-10-29 19:17:47 -0500

    user avatar construction

--- controllers/userAvatar.js
+++ controllers/userAvatar.js
@@ -3,34 +3,71 @@ const userAvatarRouter = express.Router()
 const userImage = require('../models/userImage')
 const mongoose = require("mongoose");
 const multer = require('multer')
+const aws = require( 'aws-sdk' );
+const multerS3 = require( 'multer-s3' );
 
-const Storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/');
-      },
-    filename(req, file, cb) {
-        cb(null, Date.now() + file.originalname)
-    },
-  })
+// const Storage = multer.diskStorage({
+//     destination: function(req, file, cb) {
+//         cb(null, 'uploads/');
+//       },
+//     filename(req, file, cb) {
+//         cb(null, Date.now() + file.originalname)
+//     },
+//   })
 
+
+// const fileFilter = (req, file, cb) => {
+//     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+//         cb(null, true)
+//     } else {
+//         cb(null, false)
+//     }
+// }
+
+
+// const upload = multer({
+//     storage: Storage,
+//     limits: {
+//         fileSize: 1024 * 1024 * 5
+//     },
+//     fileFilter: fileFilter
+
+// })
+
+const s3 = new aws.S3({
+	accessKeyId: process.env.ACCESSKEYID,
+	secretAccessKey: process.env.SECRETACCESSKEY,
+	Bucket: process.env.USERAVATABUCKET
+
+});
+
+const Storage = multerS3({
+    s3:s3,
+    bucket: process.env.USERAVATABUCKET,
+    acl: 'public-read',
+    key:  function(req, file, cb) {
+            cb(null, Date.now() + file.originalname)
+        }
+})
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
         cb(null, true)
     } else {
         cb(null, false)
     }
-}
+  }
 
 
-const upload = multer({
+
+  const upload = multer({
     storage: Storage,
     limits: {
-        fileSize: 1024 * 1024 * 5
+        fileSize: { fileSize: 5000000 }
     },
     fileFilter: fileFilter
-
-})
+  
+  })
 
 userAvatarRouter.get('/upload/:id', async (req, res) =>{
     id = req.params.id
@@ -39,7 +76,7 @@ userAvatarRouter.get('/upload/:id', async (req, res) =>{
     }
      try {
         const  userProfile = await userImage.find({userId: req.user._id})
-        res.status(200).json(userProfile)
+        return res.status(200).json(userProfile)
      } catch (err) {
             res.status(500).json({error: err.message})
      }
@@ -50,8 +87,8 @@ userAvatarRouter.get('/upload/:id', async (req, res) =>{
 
 
 
-userAvatarRouter.post('/upload', upload.single('userimage'), async (req, res) => {
-//     console.log('file', req.file)
+userAvatarRouter.post('/upload', upload.single('userImage'), async (req, res) => {
+    console.log('file', req.file)
         console.log('body', req.body)
 //     res.status(200).json({
 //     message: 'success!',
@@ -63,10 +100,10 @@ userAvatarRouter.post('/upload', upload.single('userimage'), async (req, res) =>
      try {
          const userProfile = new userImage ({
             userId: req.user._id,
-            userimage: req.file.path
+            userImage: req.file.location
          })
          await userProfile.save()
-         res.status(200).json(userProfile)
+         return res.status(200).json(userProfile)
      } catch (err) {
          res.status(500).json({ error: err.message})
      }
